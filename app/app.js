46e1d63a3b44ff054fc6f48b78a8ef897eb12316
Maude <mualth@gmail.com>
2017-07-17 01:18:27 -0400

    set up react framework. site still not routed quite right but will fix next

---
+++ app/app.js
@@ -0,0 +1,9 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(
+    <div>
+        <h1>Making sure this works </h1>
+    </div>
+, document.getElementById('app')
+);
