0a042f1a610362d3b0a41ab434f0f23c911586b7
Ian-Maynard <maynard.ian@gmail.com>
2017-07-20 19:16:09 -0400

    Multiple additions the server file

---
+++ server.js
@@ -0,0 +1,71 @@
+// Project Sideline 
+
+
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Models 
+var Family = require("./models/family.js");
+var Coaches = require("./models/coaches.js");
+var Events = require("./models/events.js");
+
+var app = express(); // Init & config  express 
+app.use(logger("dev:"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static("public"));
+
+mongoose.connect("mongod://localhost/sideline");
+var db = mongoose.connection;
+db.on("error", function(error) {
+    console.log("mongoose Error: ",error);
+});
+
+db.once("open",function() {
+    console.log("Mongoose Connection Successful.")
+});
+
+// Routes 
+
+
+// Getters
+        app.get("/family",function(req,res) {
+            Family.find({}, function(error, doc){
+                    if (error){
+                        res.send(error);
+                    } // On Family error 
+                    else {
+                        res.send(doc);
+                    }
+            }); // Find 
+        });  // Family get routes
+
+         app.get("/coaches",function(req,res) {
+            Coaches.find({}, function(error, doc){
+                    if (error){
+                        res.send(error);
+                    } // On Family error 
+                    else {
+                        res.send(doc);
+                    }
+            }); // Find 
+        });  // Coaches get routes   
+
+        app.get("/events",function(req,res) {
+            Events.find({}, function(error, doc){
+                            if (error){
+                                res.send(error);
+                            } // On Family error 
+                            else {
+                                res.send(doc);
+                            }
+                    }); // Find 
+         });  // events get routes   
+
+        
+        // Listen on Port 3000
+app.listen(8075, function() {
+  console.log("App running on port 8075!");
+});
